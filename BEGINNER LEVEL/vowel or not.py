ae9ae5fbcc07f0cb58c321efefe1deb6af0b2b22
nirmal619 <37336801+nirmal619@users.noreply.github.com>
2018-03-18 15:51:52 +0530

    Create vowel or not.py

---
+++ BEGINNER LEVEL/vowel or not.py
@@ -0,0 +1,13 @@
+b=input()
+if(b=='a'):
+  print("vowel")
+elif(b=='e'):
+  print("vowel")
+elif(b=='i'):
+  print("vowel")
+elif(b=='o'):
+  print("vowel")
+elif(b=='u'):
+  print("vowel")
+else:
+  print("not a vowel")
