3a97e6e54be0c800c17192df0d688a81f8bd1e76
nirmal619 <37336801+nirmal619@users.noreply.github.com>
2018-03-18 16:28:18 +0530

    Update greatest of 3 num.py

--- BEGINNER LEVEL/greatest of 3 num.py
+++ BEGINNER LEVEL/greatest of 3 num.py
@@ -1,9 +1,13 @@
-a=int(input())
-b=int(input())
-c=int(input())
-if(a>b and a>c):
-  print("a is greater")
-elif(b>a and b>c):
-  print("b is greater")
-elif(c>b and c>a):
-  print("c is greater")
+try:
+  a=int(input())
+  b=int(input())
+  c=int(input())
+except:
+  print("invalid code")
+else:
+  if(a>b and a>c):
+    print("a is greater")
+  elif(b>a and b>c):
+    print("b is greater")
+  elif(c>b and c>a):
+    print("c is greater")
