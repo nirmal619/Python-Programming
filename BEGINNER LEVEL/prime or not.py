1dbf53c8ac4124bea86f0bdb1273982e6a4a2e8a
nirmal619 <37336801+nirmal619@users.noreply.github.com>
2018-03-21 12:24:40 +0530

    Update prime or not.py

--- BEGINNER LEVEL/prime or not.py
+++ BEGINNER LEVEL/prime or not.py
@@ -2,8 +2,8 @@ a=int(input())
 for i in range(2,a+1):
   if(a%i==0):
     if(a==i):
-      print(" prime")
+      print("yes")
       break
     else:
-      print("not a prime")
+      print("no")
       break
